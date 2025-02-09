160e52d3a8674e5b30579ca4f34641de5453270d
fxrhannn <dangerfsgaming@gmail.com>
2025-02-09 22:29:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function init() {
 
     // Load 3D Teddy Model
     const loader = new THREE.GLTFLoader();
-    loader.load('https://cdn.jsdelivr.net/gh/KhronosGroup/glTF-Sample-Models/2.0/ToyBear/glTF-Binary/ToyBear.glb', function(gltf) {
+    loader.load('teddy_bear.glb', function(gltf) {
         teddy = gltf.scene;
         teddy.scale.set(1.5, 1.5, 1.5);
         scene.add(teddy);
